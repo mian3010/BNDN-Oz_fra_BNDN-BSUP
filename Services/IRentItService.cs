7736ec004d8ac69470f3e41968387493f2f6885e
Drescher86 <mdre@itu.dk>
2013-03-14 16:05:09 +0100

    Changed service name and added a few contracts/methods

---
+++ Services/IRentItService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RentIt
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRentItService
+    {
+        #region Authentication
+
+        [OperationContract]
+        [WebGet(UriTemplate="/auth",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        string Authorise(string USERNAME, string PASSWORD);
+
+        #endregion
+
+        #region Accounts
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/accounts",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/acounts/{USERNAME}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        string GetData(int value);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            UriTemplate = "/acounts/{USERNAME}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        string GetData(int value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            UriTemplate = "/accounts/{USERNAME}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        string GetData(int value);
+
+        #endregion
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
--- Services/RentItService.svc.cs
+++ Services/RentItService.svc.cs
@@ -5,16 +5,25 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using RentIt.Auth;
 
 namespace RentIt
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
-    {
-        public string GetData(int value)
+    public class RentItService : IRentItService
+    { 
+
+        public string Authorise(string user, string password)
         {
-            return string.Format("You entered: {0}", value);
+            try
+            {
+                ControlledAuth.authenticate(user, password);
+            }
+            catch (Exception e)
+            {
+
+            }
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
