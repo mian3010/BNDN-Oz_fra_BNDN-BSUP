b31ee0ae42813c7372fc1a65ceb9ba943c71304f
RentIt27 Server <RentIt27@Server>
2013-03-14 09:27:01 +0100

    changed name og the DB-folder, to DB. - Kruger

---
+++ DB/DB.fs
@@ -0,0 +1,30 @@
+﻿namespace RentIt
+
+open System
+open System.Data
+
+    module DB =
+
+        module internal Internal =
+
+            let server = "(localdb)\Projects"
+            let database = "test"
+
+            let connDB = new SqlClient.SqlConnection("server=" + server + ";Integrated Security=True;Database=" + database)
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        type test = { 
+            Id:int}
+
+        let testRead = seq {
+            let sql  = "SELECT * FROM Table1" // Dummy SQL
+            let cmd = new SqlClient.SqlCommand(sql, Internal.connDB)
+            //let cmd = Internal.connDB?GetProducts
+            Internal.connDB.Open()
+            let reader = cmd.ExecuteReader()
+
+            while reader.Read() do
+                yield { 
+                    Id = unbox (reader.["Id"])}
+                
+            Internal.connDB.Close() }
