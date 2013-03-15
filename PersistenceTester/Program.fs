2a9c4ccda55fa4ab3b769f97e10ca7920edff1de
Michael Soeby Andersen <michael@mileratec.com>
2013-03-15 14:20:44 +0100

    Get method in persistence done for now. See PersistenceTester for how to use it

---
+++ PersistenceTester/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+namespace RentIt
+
+module Main = 
+    [<EntryPoint>]
+    let main argv = 
+        let result = Persistence.Get "user" [{field="Username";operator="=";value="tumpe"}]
+        let rec outString (inResult:Map<string,string> List) = 
+         match inResult with
+          | [] -> ""
+          | x::xs -> (Map.find "Username" x) + "\n\r" + outString xs
+        printfn "%A" (outString result)
+        0 // return an integer exit code
