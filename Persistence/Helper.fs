50301ff75c7180c56b2d390cc34dac9831bbe793
Claus L. Henriksen <clih@itu.dk>
2013-03-20 16:28:15 +0100

    helper readded

--- Persistence/Helper.fs
+++ Persistence/Helper.fs
@@ -6,3 +6,17 @@ open System.Data
         let internal performSql sql = 
             let cmd = new SqlClient.SqlCommand(sql, Settings.connDb)
             cmd.ExecuteReader()
+
+        // Read a row
+        let internal extractRow (reader:SqlClient.SqlDataReader) data =
+            let rec readData = function
+                  | 0, d -> d
+                  | i, (d:Map<string,string>) -> readData ((i-1), (d.Add(reader.GetName(i-1), (reader.GetValue(i-1).ToString()))))
+            readData ((reader.FieldCount), data)
+
+        // Read rows
+        let internal extractData (reader:SqlClient.SqlDataReader) = 
+            [ while reader.Read() do
+                let start:Map<string,string> = Map.empty
+                yield (extractRow reader start)
+            ]
