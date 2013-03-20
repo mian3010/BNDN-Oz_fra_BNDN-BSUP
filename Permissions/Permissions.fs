f15fa5c96ec7de1c295d345ffd75d3fa3cea9faf
Claus L. Henriksen <clih@itu.dk>
2013-03-20 16:28:29 +0100

    Permissions work with new Persistence API

--- Permissions/Permissions.fs
+++ Permissions/Permissions.fs
@@ -19,7 +19,7 @@ open RentIt
     let CheckUserPermission (id:Identity) (permission:string) (tp:Target) :bool =
       let perm = match tp with
                  | Type x -> permission + "TYPE_" + x
-                 | Own -> perm <- permission + "OWN"
+                 | Own -> permission + "OWN"
                  | _ -> permission
 
       match id with
@@ -29,74 +29,96 @@ open RentIt
     // Assign a Permission to an ActionGroup
     // Takes ActionGroup Name and Permission. Both as strings
     let AssignPermissionToActionGroup (name:string) (permission:string) =
-      let mutable data:Map<string,string> = Map.empty
-      data <- data.Add("ActionGroup_name", name)
-      data <- data.Add("AllowedAction_name", permission)
-
-      Persistence.Insert "ActionGroup_has_AllowedAction" data
-
+      let objectName = "ActionGroup_has_AllowedAction"
+      let fieldProcessor = Persistence.Field.Default
+      let dataQ = Persistence.DataIn.createDataIn []    objectName "ActionGroup_name" name
+      let dataQ = Persistence.DataIn.createDataIn dataQ objectName "AllowedAction_name" permission
+      
+      Persistence.Api.create objectName dataQ
+      
     // Unassign a Permission to an ActionGroup
     // Takes ActionGroup Name and Permission. Both as strings
     let UnassignPermissionFromActionGroup (name:string) (permission:string) = 
-      Persistence.Delete "ActionGroup_has_AllowedAction" [{field="ActionGroup_name";operator="=";value=name}; {field="AllowedAction_name";operator="=";value=permission}]
+      let objectName = "ActionGroup_has_AllowedAction"
+      let filtersQ = Persistence.Filter.createFilter [] "ActionGroup_has_AllowedAction" "ActionGroup_name" "=" name
+      let filtersQ = Persistence.Filter.createFilter filtersQ "ActionGroup_has_AllowedAction" "AllowedAction_name" "=" permission
+      
+      Persistence.Api.delete objectName filtersQ
 
     // Add Allowed Action
     // Takes Action Name and Description. Both strings.
     let AddAllowedAction (name:string) (desc:string) =
-      let mutable data:Map<string,string> = Map.empty
-      data <- data.Add("Name", name)
-      data <- data.Add("Description", desc)
+      let objectName = "AllowedAction"
+      let dataQ = Persistence.DataIn.createDataIn []    objectName "Name" name
+      let dataQ = Persistence.DataIn.createDataIn dataQ objectName "Description" desc
       
-      Persistence.Insert "AllowedAction" data
+      Persistence.Api.create objectName dataQ
 
     // Delete Allowed Action
     // Takes Allowed Action Name
     let DeleteAllowedAction (name:string) =
-      Persistence.Delete "AllowedAction" [{field="Name";operator="=";value=name}]
+      let objectName = "AllowedAction"
+      let filtersQ = Persistence.Filter.createFilter [] objectName "Name" "=" name
+
+      Persistence.Api.delete objectName filtersQ
 
     // Add Action Group
     // Takes Name and Description
     let AddActionGroup (name:string) (desc:string) =
-      let mutable data:Map<string,string> = Map.empty
-      data <- data.Add("Name", name)
-      data <- data.Add("Description", desc)
+      let objectName = "ActionGroup"
+      let dataQ = Persistence.DataIn.createDataIn []    objectName "Name" name
+      let dataQ = Persistence.DataIn.createDataIn dataQ objectName "Description" desc
       
-      Persistence.Insert "ActionGroup" data
+      Persistence.Api.create objectName dataQ
 
     // Delete Action Group
     // Takes Action Group Name
     let DeleteActionGroup (name:string) =
-      Persistence.Delete "ActionGroup" [{field="Name";operator="=";value=name}]
+      let objectName = "ActionGroup"
+      let filtersQ = Persistence.Filter.createFilter [] objectName "Name" "=" name
+
+      Persistence.Api.delete objectName filtersQ
 
     // Assign Action Group to User Type
     // Takes User Type Name and Action Group Name
     let AssignActionGroupToUserType (userType:string) (actionGroup:string) = 
-      let mutable data:Map<string,string> = Map.empty
-      data <- data.Add("UserType_name", userType)
-      data <- data.Add("ActionGroup_name", actionGroup)
+      let objectName = "UserType_has_ActionGroup"
+      let dataQ = Persistence.DataIn.createDataIn []    objectName "UserType_name" userType
+      let dataQ = Persistence.DataIn.createDataIn dataQ objectName "ActionGroup_name" actionGroup
       
-      Persistence.Insert "UserType_has_ActionGroup" data
+      Persistence.Api.create objectName dataQ
 
     // Unassign Action Group to User Type
     // Takes User Type Name and Action Group Name
     let UnassignActionGroupToUserType (userType:string) (actionGroup:string) = 
-      Persistence.Delete "UserType_has_ActionGroup" [{field="UserType_name";operator="=";value=userType}; {field="ActionGroup_name";operator="=";value=actionGroup}]
+      let objectName = "UserType_has_ActionGroup"
+      let filtersQ = Persistence.Filter.createFilter []       objectName "UserType_name" "=" userType
+      let filtersQ = Persistence.Filter.createFilter filtersQ objectName "ActionGroup_name" "=" actionGroup
+
+      Persistence.Api.delete objectName filtersQ
 
     // Add User Type
     // Takes User Type Name
     let AddUserType (name:string) = 
-      let mutable data:Map<string,string> = Map.empty
-      data <- data.Add("Name", name)
-      
-      Persistence.Insert "UserType" data
+      let objectName = "UserType"
+      let dataQ = Persistence.DataIn.createDataIn [] objectName "Name" name
+
+      Persistence.Api.create objectName dataQ
 
     // Delete User Type
     // Takes User Type Name
     // Returns false if a User has User Type
     let DeleteUserType (name:string) :bool =
-      // Check if in use
-      let result = Persistence.Get "User" [{field="Type_name";operator="=";value=name}]
-      if not result.IsEmpty then false
+      let objectName = "User"
+      let fieldsQ = Persistence.ReadField.createReadField [] objectName "Type_name" 
+      let joinsQ = []
+      let filtersQ = Persistence.Filter.createFilter [] objectName "Type_name" "=" name
+      let readR = Persistence.Api.read fieldsQ objectName joinsQ filtersQ
+      
+      if not readR.IsEmpty then false
       else
-        // Delete
-        Persistence.Delete "UserType" [{field="Name";operator="=";value=name}]
+        let objectName = "UserType"
+        let filtersQ = Persistence.Filter.createFilter [] objectName "Name" "=" name
+        Persistence.Api.delete objectName filtersQ |> ignore
+        true //TODO Add check
+      
