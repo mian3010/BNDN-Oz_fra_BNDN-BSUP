e5f90549db32a939ef0311266147d104bc990aab
Claus L. Henriksen <clih@itu.dk>
2013-03-24 09:47:15 +0100

    Adding tests

--- ProductTest/TestMake.fs
+++ ProductTest/TestMake.fs
@@ -5,15 +5,62 @@ module ProductTest =
   open FsUnit.Xunit
 
   type Product = ProductTypes.Product
+  //exception ArgumentException = RentIt.Product.ArgumentException
 
   let userId = "2"
   let name = "test product"
   let productType = "Movie"
-  let description = "desc"
-  let buyPrice = 12
-  let rentPrice = 5
+  let description = Some "desc"
+  let buyPrice = Some 12
+  let rentPrice = Some 5
 
   [<Fact>]
   let ``buyPrice should work``() =
-    let p = Product.make userId name productType (Some description) (Some buyPrice) (Some rentPrice)
-    p.buyPrice.Value |> should equal buyPrice
+    let p = Product.make userId name productType description buyPrice rentPrice
+    p.buyPrice.Value |> should equal buyPrice.Value
+
+  [<Fact>]
+  let ``rentPrice should work``() =
+    let p = Product.make userId name productType description buyPrice rentPrice
+    p.rentPrice.Value |> should equal rentPrice.Value
+
+  [<Fact>]
+  let ``owner should work``() =
+    let p = Product.make userId name productType description buyPrice rentPrice
+    p.owner |> should equal userId
+
+  [<Fact>]
+  let ``name should work``() =
+    let p = Product.make userId name productType description buyPrice rentPrice
+    p.name |> should equal name
+
+  [<Fact>]
+  let ``desc should work``() =
+    let p = Product.make userId name productType description buyPrice rentPrice
+    p.description.Value |> should equal description.Value
+
+  [<Fact>]
+  let ``type should work``() =
+    let p = Product.make userId name productType description buyPrice rentPrice
+    p.productType |> should equal productType
+
+  [<Fact>]
+  let ``owner should throw``() =
+  //  Product.make null name productType description buyPrice rentPrice |> should throw typeof<ArgumentException>
+    Product.make null name productType description buyPrice rentPrice |> should throw typeof<System.Exception>
+  
+  [<Fact>]
+  let ``name should throw``() =
+  //  Product.make null name productType description buyPrice rentPrice |> should throw typeof<ArgumentException>
+    Product.make userId null productType description buyPrice rentPrice |> should throw typeof<System.Exception>
+  
+  [<Fact>]
+  let ``productType should throw``() =
+  //  Product.make null name productType description buyPrice rentPrice |> should throw typeof<ArgumentException>
+    Product.make userId name null description buyPrice rentPrice |> should throw typeof<System.Exception>
+  
+  [<Fact>]
+  let ``productType should throw no such``() =
+  //  Product.make null name productType description buyPrice rentPrice |> should throw typeof<NoSuchProductType>
+    Product.make userId name "muhaha" description buyPrice rentPrice |> should throw typeof<System.Exception>
+      
