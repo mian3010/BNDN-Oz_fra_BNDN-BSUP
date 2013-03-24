78f4b94ad3ba944de2ef81956f5ec599738756d4
Claus L. Henriksen <clih@itu.dk>
2013-03-24 09:17:00 +0100

    Add test

---
+++ ProductTest/ProductTest.fs
@@ -0,0 +1,19 @@
+﻿namespace RentIt
+
+module ProductTest = 
+  open Xunit
+  open FsUnit.Xunit
+
+  type Product = ProductTypes.Product
+
+  let userId = "2"
+  let name = "test product"
+  let productType = "Movie"
+  let description = "desc"
+  let buyPrice = 12
+  let rentPrice = 5
+
+  [<Fact>]
+  let ``make that works``() =
+    let p = Product.make userId name productType (Some description) (Some buyPrice) (Some rentPrice)
+    p.buyPrice.Value |> should equal buyPrice
