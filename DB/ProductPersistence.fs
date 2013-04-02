b4dcd66503e28da03699c83b7609d1d7413874fa
Michael Soeby Andersen <michael@mileratec.com>
2013-04-02 12:10:15 +0200

    FUCKING CLAUS MOTHERFUCKER CHANGED THIS SHIT

--- DB/ProductPersistence.fs
+++ DB/ProductPersistence.fs
@@ -9,6 +9,37 @@ module ProductPersistence =
   exception NoSuchUser
   exception ProductNotPublished
   exception ProductAlreadyExists
+
+  let internal convertDatetimeToString (date:System.DateTime) =
+    (string date.Year)+"-"+(string date.Month)+"-"+(string date.Day)
+
+  let internal convertToDataIn objectName (prod:Product) =
+    let dataQ = ref (Persistence.DataIn.createDataIn [] objectName "Name" prod.name)
+    dataQ := Persistence.DataIn.createDataIn !dataQ objectName "Created_date" (convertDatetimeToString prod.createDate)
+    dataQ := Persistence.DataIn.createDataIn !dataQ objectName "ProductType_Name" prod.productType
+    dataQ := Persistence.DataIn.createDataIn !dataQ objectName "User_Username" prod.owner
+    dataQ := Persistence.DataIn.createDataIn !dataQ objectName "Published" (string prod.published)
+    if (prod.description.IsSome) then dataQ := Persistence.DataIn.createDataIn !dataQ objectName "Description" prod.description.Value
+    if (prod.rentPrice.IsSome) then dataQ := Persistence.DataIn.createDataIn !dataQ objectName "Rent_price" (string prod.rentPrice.Value)
+    if (prod.buyPrice.IsSome) then dataQ := Persistence.DataIn.createDataIn !dataQ objectName "Buy_price" (string prod.buyPrice.Value)
+    dataQ
+
+  let internal convertMetadataFromResult (result:Map<string,string> List) =
+    let metadataMap:Map<string,ProductTypes.Meta> = Map.empty
+    for row in result do
+      metadataMap.Add 
+
+  let internal getMetaData productId =
+    let objectName = "MetaData"
+    let fieldsQ = ref (Persistence.ReadField.createReadField [] objectName "Content")
+    fieldsQ := Persistence.ReadField.createReadField !fieldsQ objectName "MetaDataType_Name"
+    let joinsQ = []
+    let filtersQ = Persistence.Filter.createFilter [] objectName "Product_Id" "=" (string productId)
+    let readR = Persistence.Api.read !fieldsQ objectName joinsQ filtersQ
+    convertMetadataFromResult readR
+
+  let internal convertFromResult (result:Map<string,string> List) =
+    
   
   /// <summay>
   /// Creater in persistence layer
@@ -19,8 +50,10 @@ module ProductPersistence =
   /// <exception> NoSuchUser </exception>
   /// <exception> NoSuchProductType </exception>
   let createProduct (p:Product) : Product =
-    raise (new System.NotImplementedException())
-
+    let objectName = "Product"
+    let dataQ = convertToDataIn objectName p
+    let updateR = Persistence.Api.create objectName dataQ
+    convertFromResult updateR
   /// <summay>
   /// Update an exsisting product
   ///</summary>
@@ -88,3 +121,4 @@ module ProductPersistence =
   /// <exception> NoSuchProduct </exception>
   let publishProduct (pId:string) (status:bool) =
     raise (new System.NotImplementedException())
+  
