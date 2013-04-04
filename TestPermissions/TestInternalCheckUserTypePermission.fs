a78bef06d9bf473e2e7a3f837724d30d961feb25
Claus L. Henriksen <clih@itu.dk>
2013-04-04 22:46:17 +0200

    Added TestPermissions

---
+++ TestPermissions/TestInternalCheckUserTypePermission.fs
@@ -0,0 +1,11 @@
+﻿namespace RentIt.Test
+
+module ProductTest = 
+  open Xunit
+  open FsUnit.Xunit
+  open RentIt
+
+  [<Fact>]
+  let ``check user type permission should false``() =
+    let allowed = Permissions.checkUserTypePermission "Admin" "not a permission found in db"
+    allowed |> should equal false
