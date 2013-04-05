ebf177cdb5533fb8764ee95e55e124ae1e4ef465
Nicolai Krüger <nicolaikr@hotmail.com>
2013-04-05 03:59:50 +0200

    Removed unused "usings"

--- Services/IRentItService.cs
+++ Services/IRentItService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
-using System.Text;
 using System.IO;
-using System.Web.Services;
 
 namespace RentIt
 {
